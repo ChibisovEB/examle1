b238d51e5028beec5e89ffd445c24362f5ba28d6
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-02-11 12:11:58 +0500

    Create Programs.cs

---
+++ Programs.cs
@@ -0,0 +1,50 @@
+
+    // //  Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+    // //  какое число большее, а какое меньшее. 
+    // Console.Write("Enter one namber: ");
+    // int namber1 = Convert.ToInt32(Console.ReadLine());
+    // Console.Write("Enter two namber: ");
+    // int namber2 = Convert.ToInt32(Console.ReadLine());
+    // if (namber1 > namber2)
+    //     Console.WriteLine($"The first number {namber1} is greater than the second {namber2}");
+    // else
+    //     Console.WriteLine($"The second number {namber2} is greater than the first {namber1}");
+        
+        
+    // //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+    // Console.Write("Enter one namber: ");
+    // int namber1 = Convert.ToInt32(Console.ReadLine());
+    // Console.Write("Enter two namber: ");
+    // int namber2 = Convert.ToInt32(Console.ReadLine());
+    // Console.Write("Enter three namber: ");
+    // int namber3 = Convert.ToInt32(Console.ReadLine());
+    // if ((namber1 > namber2) & (namber1 > namber3))
+    //     Console.WriteLine(namber1);
+    // else if ((namber2 > namber1) & (namber2 > namber3))
+    //     Console.WriteLine(namber2);
+    // else
+    //     Console.WriteLine(namber3);
+        
+    // // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+    // // является ли число чётным (делится ли оно на два без остатка).    
+    // Console.Write("Enter namber: ");
+    // int namber = Convert.ToInt32(Console.ReadLine()); 
+    // if (namber % 2 == 0)
+    //     Console.WriteLine("The number is even");
+    // else
+    //     Console.WriteLine("The number is not even");
+    
+    // // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе 
+    // // показывает все чётные числа от 1
+    // Console.Write("Enter namber: ");
+    // int namber = Convert.ToInt32(Console.ReadLine()); 
+    // int index = 1;
+    // string comma = ", ";
+    // while (index <= namber) 
+    // {
+    //     if (index == namber) comma = "";                        // Эти две строчки нужны для
+    //     if ((index == namber-1) & (index % 2 == 0)) comma = ""; // исключения лишней запятой
+    //     if (index % 2 == 0)
+    //         Console.Write(index + comma);
+    //     index++;
+    // }
