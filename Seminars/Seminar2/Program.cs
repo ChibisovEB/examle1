a1fed70d2278d13a40946c0ac35d9a62f4089c20
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-02-15 06:57:43 +0000

    Task 10 - final

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,19 +1,17 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
 // на выходе показывает вторую цифру этого числа.
-int GetTwoDigit(int number)
-{
-    return (number / 10) % 10;
-}
+   int GetTwoDigit(int number)
+   {
+       return (number / 10) % 10;
+   }
+        
+   string message = "Enter a three digit number: ";
+   int number = 0;
+   while (number<100 || number>999)
+        {
+            Console.Write(message);    
+            number = Convert.ToInt32(Console.ReadLine());
+            message = "I insist, enter a three-digit number:";
+        }
 
-// 
-Console.Write("Enter a three digit number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number<100 || number>999)
-{
-    Console.WriteLine("Тут бы сделать цикл while заставляющий пользователя вводить число пока он не введет трехзначное");
-    Console.WriteLine("но ночь уже, а потом переделывать неохото будет");
-    goto Exit;
-}
-Console.WriteLine($"The second digit of the number {number} is {GetTwoDigit(number)}");
-
-Exit:;
+   Console.WriteLine($"The second digit of the number {number} is {GetTwoDigit(number)}");
