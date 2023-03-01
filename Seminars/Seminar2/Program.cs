e8765da676f0fceb71aa6e40406ae31d864eaebe
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-03-01 18:05:23 +0700

    tasks 34, 36, 38

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,56 +1,75 @@
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
-// на выходе показывает вторую цифру этого числа.
-//   int GetTwoDigit(int number)
-//   {
-//       return (number / 10) % 10;
-//   }
-//        
-//   string message = "Enter a three digit number: ";
-//   int number = 0;
-//   while (number<100 || number>999)
-//        {
-//            Console.Write(message);    
-//            number = Convert.ToInt32(Console.ReadLine());
-//            message = "I insist, enter a three-digit number:";
-//        }
-//   Console.WriteLine($"The second digit of the number {number} is {GetTwoDigit(number)}");
+// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Задача 13: Напишите программу, которая выводит третью цифру 
-// заданного числа или сообщает, что третьей цифры нет.
-//        int GetThirdDigit(int num)
-//        {
-//            // Метод возвращает третью цифру из числа  num
-//            // Если число состоит из 2 и менее цифр возвращается -1
-//            // Сдесь никак не обрабатываются отрицательные числа и строки по скольку в условии задачи не было об этом сказано,
-//            // Вообще о таких вещах надо задавать вопросы заказчику до начала всех работ.
-//            if (num<100) return -1;
-//            while (num>999) num = num / 10;
-//            return (num % 10);
-//        }
-//        
-//        Console.Write("Enter a number:");    
-//        int number = Convert.ToInt32(Console.ReadLine());
-//        int thirdNumber = GetThirdDigit(number);
-//
-//        if (thirdNumber == -1)
-//            Console.WriteLine("There is no third digit in your number");
-//        else
-//            Console.WriteLine($"The third digit of the number {number} is {thirdNumber}");
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Задача 15: Напишите программу, которая принимает на вход цифру, 
-// обозначающую день недели, и проверяет, является ли этот день выходным.
-//    bool isWeekend(int num)
-//    {
-//        // Тут тоже было бы неплохо проверить на принадлежность числа к дням недели, но в условии задачи этого нет
-//        // Да и вообще возможно это реализовано в другом методе другими специально обучеными людьми
-//        if (num == 6 || num == 7) return true; else return false;
-//    }
-//        
-//    Console.Write("Enter the number of the day of the week: ");    
-//    int numberDay = Convert.ToInt32(Console.ReadLine());
-//    
-//    if (isWeekend(numberDay))
-//        Console.WriteLine("Yes");
-//    else
-//        Console.WriteLine("No");
+// int[] CreateArray(int Len)
+// {
+//     int[] myArray = new int[Len];
+//     for (int i = 0; i <= myArray.Length - 1; i++)
+//     {
+//         myArray[i] = new Random().Next(100, 1000);
+//     }
+//     return myArray;
+// }
+
+// int SumEvenElements(int[] myAr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= myAr.Length - 1; i++)
+//     {
+//         if (myAr[i]%2 == 0) sum++;
+//     }
+//     return sum;
+// }
+
+// int[] array = CreateArray(67);
+// Console.WriteLine(SumEvenElements(array));
+
+// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// // Найдите сумму элементов, стоящих на нечётных позициях.
+//  int[] CreateArray(int Len)
+// {
+//     int[] myArray = new int[Len];
+//     for (int i = 0; i <= myArray.Length - 1; i++)
+//     {
+//         myArray[i] = new Random().Next(1, 100);
+//     }
+//     return myArray;
+// }
+
+// int SumEvenElements(int[] myAr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= myAr.Length - 1; i++)
+//         if (i%2 != 0) sum += myAr[i];
+//     return sum;
+// }
+// int[] array = CreateArray(10);
+// Console.WriteLine(SumEvenElements(array));
+
+
+// // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// // и минимальным элементов массива.
+// int[] CreateArray(int Len)
+// {
+//     int[] myArray = new int[Len];
+//     for (int i = 0; i <= myArray.Length - 1; i++)
+//     {
+//         myArray[i] = new Random().Next(-10, 10);
+//     }
+//     return myArray;
+// }
+
+// void printArray(int[] myAr)
+// {
+//     for (int i = 0; i <= myAr.Length - 1; i++)
+//         Console.Write(myAr[i] + " ");
+//     Console.WriteLine();
+// }
+
+// int[] array = CreateArray(10);
+// int minElement = array.Min();
+// int maxElement = array.Max();
+// printArray(array);
+// Console.WriteLine($"min = {minElement}");
+// Console.WriteLine($"max = {maxElement}");
+// Console.WriteLine($"difference = {maxElement - minElement}");
