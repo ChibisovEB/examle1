84a0f2f5cff9c2d791b76abbf1351bcff6717a81
ChibisovEB <124187608+ChibisovEB@users.noreply.github.com>
2023-03-03 17:44:59 +0700

    Task 41, 43

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,75 +1,70 @@
-// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// // Напишите программу, которая покажет количество чётных чисел в массиве.
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Enter the number of numbers: ");
+int col = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < col; i++)
+{
+    Console.Write($"Enter {i+1} number:");
+    if (Convert.ToInt32(Console.ReadLine()) > 0)
+        count++;
+}
+Console.WriteLine($"Count = {count}");
 
-// int[] CreateArray(int Len)
-// {
-//     int[] myArray = new int[Len];
-//     for (int i = 0; i <= myArray.Length - 1; i++)
-//     {
-//         myArray[i] = new Random().Next(100, 1000);
-//     }
-//     return myArray;
-// }
+/////////////////////////////////////////////////////////////
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
 
-// int SumEvenElements(int[] myAr)
-// {
-//     int sum = 0;
-//     for (int i = 0; i <= myAr.Length - 1; i++)
-//     {
-//         if (myAr[i]%2 == 0) sum++;
-//     }
-//     return sum;
-// }
+const int COEFF = 0;
+const int CONSTANT= 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+const int X = 0;
+const int Y = 1;
 
-// int[] array = CreateArray(67);
-// Console.WriteLine(SumEvenElements(array));
+double[] EnterLine(int numberOFLine)
+{
+    double[] lineData = new double[2];
+    Console.Write($"Enter the coefficient for the {numberOFLine} straight: ");
+    lineData[COEFF] = Convert.ToDouble(Console.ReadLine());
+    Console.Write($"Enter the constant for the {numberOFLine} straight: ");
+    lineData[CONSTANT] = Convert.ToDouble(Console.ReadLine());
+    return lineData;
+}
+double[] FindCoordinates(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X] = (lineData1[CONSTANT] - lineData2[CONSTANT]) 
+        / (lineData2[COEFF] - lineData1[COEFF]);
+    coord[Y] = lineData1[CONSTANT] * coord[X] + lineData1[CONSTANT];
+    return coord;
+}
+bool CheckLine(double[] lineData1, double[] lineData2)
+{
+    if (lineData1[COEFF] == lineData2[COEFF])
+    {
+        if (lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("The lines coincide!");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("The lines are parallel!");
+            return false;
+        }
+    }
+    return true;
+}
 
-// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-// // Найдите сумму элементов, стоящих на нечётных позициях.
-//  int[] CreateArray(int Len)
-// {
-//     int[] myArray = new int[Len];
-//     for (int i = 0; i <= myArray.Length - 1; i++)
-//     {
-//         myArray[i] = new Random().Next(1, 100);
-//     }
-//     return myArray;
-// }
+double[] lineData1 = EnterLine(LINE1);
+double[] lineData2 = EnterLine(LINE2);
 
-// int SumEvenElements(int[] myAr)
-// {
-//     int sum = 0;
-//     for (int i = 0; i <= myAr.Length - 1; i++)
-//         if (i%2 != 0) sum += myAr[i];
-//     return sum;
-// }
-// int[] array = CreateArray(10);
-// Console.WriteLine(SumEvenElements(array));
+if (CheckLine(lineData1, lineData2))
+{
+    double[] coord = FindCoordinates(lineData1, lineData2);
+    Console.Write($"Point of intersection of lines y={lineData1[COEFF]}*x+{lineData1[CONSTANT]} и y={lineData2[COEFF]}*x+{lineData2[CONSTANT]}");
+    Console.WriteLine($" has coordinates ({coord[X]}, {coord[Y]})");
+}
 
-
-// // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным 
-// // и минимальным элементов массива.
-// int[] CreateArray(int Len)
-// {
-//     int[] myArray = new int[Len];
-//     for (int i = 0; i <= myArray.Length - 1; i++)
-//     {
-//         myArray[i] = new Random().Next(-10, 10);
-//     }
-//     return myArray;
-// }
-
-// void printArray(int[] myAr)
-// {
-//     for (int i = 0; i <= myAr.Length - 1; i++)
-//         Console.Write(myAr[i] + " ");
-//     Console.WriteLine();
-// }
-
-// int[] array = CreateArray(10);
-// int minElement = array.Min();
-// int maxElement = array.Max();
-// printArray(array);
-// Console.WriteLine($"min = {minElement}");
-// Console.WriteLine($"max = {maxElement}");
-// Console.WriteLine($"difference = {maxElement - minElement}");
